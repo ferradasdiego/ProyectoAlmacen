1e464768c385c6edf540f7d70a075ee5aeb17260
ferradasdiego <ies.a15diegocf@gmail.com>
2017-12-11 18:59:12 +0100

    Proyecto documentado

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+/**
+ * @file Aplicación para la gestión de un almacén
+ * @author Diego Costas
+ * @version 1.0
+ */
 
+/**
+ * @class almacen
+ * @property {number} capacidad Número de artículos que puede haber en el almacén
+ * @prop {array} cds array con los cds del almacén
+ * @prop {array} dvds array con los dvds del almacén
+ */
 class Almacen {
     //Constructor de la clase almacén, recibe la cantidad de elementos (cds o dvds) que puede haber. 
     constructor(capacidad) {
@@ -6,6 +17,12 @@ class Almacen {
         this.cds = [];
         this.dvds = [];
     }
+
+/**
+     * @description función para introducir artículos en el almacén
+     * @param {object} nombre del articulo que queremos insertar en el almacén
+     * @returns {number} devuelve el número de artículos que se meten en el almacén
+     */
     //El método introducir recibe un objeto y si hay sitio, tras comprobar si es de tipo cd o dvd lo mete dentro de la colección (Array) correspondiente. Devuelve 0 si no puede meterlo o en caso contrario el número de unidades introducidas.
     introducir(objeto) {
         if (this.capacidad == 0) {
@@ -37,6 +54,12 @@ class Almacen {
             return objeto.unidades
         }
     }
+
+    /**
+     * @description función para buscar un articulo en el almacén
+     * @param {string} string con el titulo o el autor del articulo que queramos buscar
+     * @returns {object} devuelve el artículo que busquemos
+     */
     //Este método recibe una cadena de texto y busca dentro del almacén si existe un cd o dvd con ese título o ese autor, si lo encuentra devulve un objeto con el título, autor, unidades y precio. Si no lo encuentra devuelve null
     buscar(tituloautor) {
         for (let obj of this.cds) {
@@ -63,8 +86,14 @@ class Almacen {
         }
         return null;
     }
+
+    /**
+     * @description función para comprar un articulo en el almacén
+     * @param {string} titulo del articulo que queramos buscar
+     * @param {number} dinero que nos entrega el cliente
+     * @returns {number} devuelve -1 si no llega el dinero, sinó devuelve lo que le hay que devolver al cliente
+     */
     //El método comprar, recibe una cadena de texto con un título y la cantidad de dinero. Devuelve -1 si no puede comprarlo y la cantidad de dinero que le sobra si lo puede comprar.
-    
     comprar(titulo, dinero) {
         for (let obj of this.cds) {
             if (titulo == obj.titulo) {
